cd3aadee0285fea74c06f407bc4f06a4e6a2e281
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:03:27 -0300

    Update 01-Create-Tables.sql

--- 01-Fundamentos/01-Create-Tables.sql
+++ 01-Fundamentos/01-Create-Tables.sql
@@ -1,1 +1,28 @@
+CREATE TABLE Clientes (
+    ClienteID INT PRIMARY KEY,
+    Nome VARCHAR(100),
+    Cidade VARCHAR(100),
+    Email VARCHAR(100)
+);
 
+CREATE TABLE Produtos (
+    ProdutoID INT PRIMARY KEY,
+    NomeProduto VARCHAR(100),
+    Preco DECIMAL(10,2)
+);
+
+CREATE TABLE Pedidos (
+    PedidoID INT PRIMARY KEY,
+    ClienteID INT,
+    DataPedido DATE,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
+);
+
+CREATE TABLE ItensPedido (
+    ItemID INT PRIMARY KEY,
+    PedidoID INT,
+    ProdutoID INT,
+    Quantidade INT,
+    FOREIGN KEY (PedidoID) REFERENCES Pedidos(PedidoID),
+    FOREIGN KEY (ProdutoID) REFERENCES Produtos(ProdutoID)
+);
