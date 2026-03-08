4fea60cb0ea9fb434bf3a889962dd8c082825107
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:14:51 -0300

    Update 03-Consultas-Basicas.sql

--- 01-Fundamentos/03-Consultas-Basicas.sql
+++ 01-Fundamentos/03-Consultas-Basicas.sql
@@ -1,1 +1,25 @@
+-- Ver todos os clientes
+SELECT * FROM Clientes;
 
+-- Ver todos os produtos
+SELECT * FROM Produtos;
+
+-- Ver pedidos com nome do cliente
+SELECT 
+    Pedidos.PedidoID,
+    Clientes.Nome,
+    Pedidos.DataPedido
+FROM Pedidos
+INNER JOIN Clientes
+ON Pedidos.ClienteID = Clientes.ClienteID;
+
+-- Ver produtos comprados em cada pedido
+SELECT 
+    Pedidos.PedidoID,
+    Produtos.NomeProduto,
+    ItensPedido.Quantidade
+FROM ItensPedido
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID
+INNER JOIN Pedidos
+ON ItensPedido.PedidoID = Pedidos.PedidoID;
