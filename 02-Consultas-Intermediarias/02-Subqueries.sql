b5f2fcf0d4164e82161f41af2721686021a061ce
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:31:15 -0300

    Update 02-Subqueries.sql

--- 02-Consultas-Intermediarias/02-Subqueries.sql
+++ 02-Consultas-Intermediarias/02-Subqueries.sql
@@ -1,1 +1,21 @@
+-- Clientes que fizeram pedidos
+SELECT Nome
+FROM Clientes
+WHERE ClienteID IN (
+    SELECT ClienteID
+    FROM Pedidos
+);
 
+-- Produtos que foram vendidos
+SELECT NomeProduto
+FROM Produtos
+WHERE ProdutoID IN (
+    SELECT ProdutoID
+    FROM ItensPedido
+);
+
+-- Pedidos que possuem mais de um item
+SELECT PedidoID
+FROM ItensPedido
+GROUP BY PedidoID
+HAVING COUNT(*) > 1;
