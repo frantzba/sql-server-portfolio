43638086f35399bd3e3f9227cb8589c87a3dbb34
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:19:49 -0300

    Update 04-Consultas-Where.sql

--- 01-Fundamentos/04-Consultas-Where.sql
+++ 01-Fundamentos/04-Consultas-Where.sql
@@ -1,1 +1,19 @@
+-- Clientes da cidade de Lisboa
+SELECT *
+FROM Clientes
+WHERE Cidade = 'Lisboa';
 
+-- Produtos com preço maior que 50
+SELECT *
+FROM Produtos
+WHERE Preco > 50;
+
+-- Pedidos feitos depois de 2024-01-11
+SELECT *
+FROM Pedidos
+WHERE DataPedido > '2024-01-11';
+
+-- Itens com quantidade maior que 1
+SELECT *
+FROM ItensPedido
+WHERE Quantidade > 1;
