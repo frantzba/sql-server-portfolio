b6e07cf57c140b59338946a7b59aeef1e85712c4
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:40:12 -0300

    Update Desafio-01.sql

--- 03-Desafios/Desafio-01.sql
+++ 03-Desafios/Desafio-01.sql
@@ -1,1 +1,10 @@
+-- Desafio 1
+-- Mostrar o nome do cliente e quantos pedidos ele fez
 
+SELECT 
+    Clientes.Nome,
+    COUNT(Pedidos.PedidoID) AS TotalPedidos
+FROM Clientes
+INNER JOIN Pedidos
+ON Clientes.ClienteID = Pedidos.ClienteID
+GROUP BY Clientes.Nome;
