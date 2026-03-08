83e1815ce06134fa2c03704c47b4899e1656b409
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:33:40 -0300

    Update 03-Views.sql

--- 02-Consultas-Intermediarias/03-Views.sql
+++ 02-Consultas-Intermediarias/03-Views.sql
@@ -1,1 +1,19 @@
+-- View para mostrar pedidos com nome do cliente
+CREATE VIEW vw_PedidosClientes AS
+SELECT 
+    Pedidos.PedidoID,
+    Clientes.Nome,
+    Pedidos.DataPedido
+FROM Pedidos
+INNER JOIN Clientes
+ON Pedidos.ClienteID = Clientes.ClienteID;
 
+
+-- View para mostrar produtos vendidos
+CREATE VIEW vw_ProdutosVendidos AS
+SELECT 
+    Produtos.NomeProduto,
+    ItensPedido.Quantidade
+FROM ItensPedido
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID;
