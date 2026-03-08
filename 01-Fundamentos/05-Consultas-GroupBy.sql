7b71744dd81ef4e60468d04396ee5fb727b6abaf
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:24:42 -0300

    Update 05-Consultas-GroupBy.sql

--- 01-Fundamentos/05-Consultas-GroupBy.sql
+++ 01-Fundamentos/05-Consultas-GroupBy.sql
@@ -1,1 +1,27 @@
+ Quantidade de pedidos por cliente
+SELECT 
+    ClienteID,
+    COUNT(PedidoID) AS TotalPedidos
+FROM Pedidos
+GROUP BY ClienteID;
 
+-- Quantidade total vendida por produto
+SELECT 
+    ProdutoID,
+    SUM(Quantidade) AS TotalVendido
+FROM ItensPedido
+GROUP BY ProdutoID;
+
+-- Número de pedidos por data
+SELECT 
+    DataPedido,
+    COUNT(*) AS NumeroPedidos
+FROM Pedidos
+GROUP BY DataPedido;
+
+-- Total de produtos vendidos por pedido
+SELECT 
+    PedidoID,
+    SUM(Quantidade) AS TotalProdutos
+FROM ItensPedido
+GROUP BY PedidoID;
