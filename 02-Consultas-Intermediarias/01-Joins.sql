9ee713ec54d91fdc9657f2504c3521f1532361fa
frantzba <frantzbaguidy7@gmail.com>
2026-03-08 13:28:30 -0300

    Update 01-Joins.sql

--- 02-Consultas-Intermediarias/01-Joins.sql
+++ 02-Consultas-Intermediarias/01-Joins.sql
@@ -1,1 +1,32 @@
+-- Mostrar pedidos com nome do cliente
+SELECT 
+    Pedidos.PedidoID,
+    Clientes.Nome,
+    Pedidos.DataPedido
+FROM Pedidos
+INNER JOIN Clientes
+ON Pedidos.ClienteID = Clientes.ClienteID;
 
+-- Mostrar produtos vendidos em cada pedido
+SELECT 
+    Pedidos.PedidoID,
+    Produtos.NomeProduto,
+    ItensPedido.Quantidade
+FROM ItensPedido
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID
+INNER JOIN Pedidos
+ON ItensPedido.PedidoID = Pedidos.PedidoID;
+
+-- Mostrar cliente, produto e quantidade comprada
+SELECT 
+    Clientes.Nome,
+    Produtos.NomeProduto,
+    ItensPedido.Quantidade
+FROM ItensPedido
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID
+INNER JOIN Pedidos
+ON ItensPedido.PedidoID = Pedidos.PedidoID
+INNER JOIN Clientes
+ON Pedidos.ClienteID = Clientes.ClienteID;
