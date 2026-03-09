267214624ecadceb59d846e53a6ae752d30d46be
frantzba <frantzbaguidy7@gmail.com>
2026-03-09 19:32:34 -0300

    Update Desafio-05.sql

--- 03-Desafios/Desafio-05.sql
+++ 03-Desafios/Desafio-05.sql
@@ -1,1 +1,11 @@
+-- Desafio 5
+-- Mostrar o valor total de vendas por produto
 
+SELECT 
+    Produtos.NomeProduto,
+    SUM(ItensPedido.Quantidade * Produtos.Preco) AS TotalVendas
+FROM ItensPedido
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID
+GROUP BY Produtos.NomeProduto
+ORDER BY TotalVendas DESC;
