a6ed212d5c2ce72b41a5c7150315605a93db5c4b
frantzba <frantzbaguidy7@gmail.com>
2026-03-09 19:26:41 -0300

    Update Desafio-03.sql

--- 03-Desafios/Desafio-03.sql
+++ 03-Desafios/Desafio-03.sql
@@ -1,1 +1,13 @@
+-- Desafio 3
+-- Mostrar quantos produtos diferentes cada cliente comprou
 
+SELECT 
+    Clientes.Nome,
+    COUNT(DISTINCT ItensPedido.ProdutoID) AS ProdutosDiferentes
+FROM Clientes
+INNER JOIN Pedidos
+ON Clientes.ClienteID = Pedidos.ClienteID
+INNER JOIN ItensPedido
+ON Pedidos.PedidoID = ItensPedido.PedidoID
+GROUP BY Clientes.Nome
+ORDER BY ProdutosDiferentes DESC;
