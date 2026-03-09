64f907741e40c81d16325d6287415400d12376b8
frantzba <frantzbaguidy7@gmail.com>
2026-03-09 19:38:12 -0300

    Update Desafio-07.sql

--- 03-Desafios/Desafio-07.sql
+++ 03-Desafios/Desafio-07.sql
@@ -1,1 +1,9 @@
+-- Desafio 7
+-- Mostrar quantos produtos diferentes existem em cada pedido
 
+SELECT 
+    PedidoID,
+    COUNT(DISTINCT ProdutoID) AS ProdutosDiferentes
+FROM ItensPedido
+GROUP BY PedidoID
+ORDER BY ProdutosDiferentes DESC;
