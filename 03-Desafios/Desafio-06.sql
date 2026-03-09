71871d1a0efc050fc4033ef9fb7e5521ec756a4e
frantzba <frantzbaguidy7@gmail.com>
2026-03-09 19:34:58 -0300

    Update Desafio-06.sql

--- 03-Desafios/Desafio-06.sql
+++ 03-Desafios/Desafio-06.sql
@@ -1,1 +1,9 @@
+-- Desafio 6
+-- Mostrar o número de pedidos feitos em cada data
 
+SELECT 
+    DataPedido,
+    COUNT(PedidoID) AS TotalPedidos
+FROM Pedidos
+GROUP BY DataPedido
+ORDER BY DataPedido;
