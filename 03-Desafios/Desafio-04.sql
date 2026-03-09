7d22d518439c7a98acddaac55c0b1998811b2e3a
frantzba <frantzbaguidy7@gmail.com>
2026-03-09 19:30:04 -0300

    Update Desafio-04.sql

--- 03-Desafios/Desafio-04.sql
+++ 03-Desafios/Desafio-04.sql
@@ -1,1 +1,11 @@
+-- Desafio 4
+-- Mostrar o cliente que fez mais pedidos
 
+SELECT 
+    Clientes.Nome,
+    COUNT(Pedidos.PedidoID) AS TotalPedidos
+FROM Clientes
+INNER JOIN Pedidos
+ON Clientes.ClienteID = Pedidos.ClienteID
+GROUP BY Clientes.Nome
+ORDER BY TotalPedidos DESC;
