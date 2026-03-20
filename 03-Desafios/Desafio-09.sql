85a75a681f4d073a3cce81eba1a2d463896a9f2c
frantzba <frantzbaguidy7@gmail.com>
2026-03-20 19:00:05 -0300

    Update Desafio-09.sql

--- 03-Desafios/Desafio-09.sql
+++ 03-Desafios/Desafio-09.sql
@@ -1,1 +1,15 @@
+-- Desafio 9
+-- Ranking de clientes por total gasto
 
+SELECT 
+    Clientes.Nome,
+    SUM(ItensPedido.Quantidade * Produtos.Preco) AS TotalGasto,
+    RANK() OVER (ORDER BY SUM(ItensPedido.Quantidade * Produtos.Preco) DESC) AS Ranking
+FROM Clientes
+INNER JOIN Pedidos
+ON Clientes.ClienteID = Pedidos.ClienteID
+INNER JOIN ItensPedido
+ON Pedidos.PedidoID = ItensPedido.PedidoID
+INNER JOIN Produtos
+ON ItensPedido.ProdutoID = Produtos.ProdutoID
+GROUP BY Clientes.Nome;
