c9b8cfa93b2916108b542293e74998b8b99d85e4
frantzba <frantzbaguidy7@gmail.com>
2026-03-28 17:39:06 -0300

    Update 02-Create-Tables.sql

--- 04-Projeto-Vendas/02-Create-Tables.sql
+++ 04-Projeto-Vendas/02-Create-Tables.sql
@@ -1,1 +1,28 @@
+CREATE TABLE Clientes (
+    ClienteID INT PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(100),
+    Email VARCHAR(100),
+    Cidade VARCHAR(100)
+);
 
+CREATE TABLE Produtos (
+    ProdutoID INT PRIMARY KEY IDENTITY(1,1),
+    Nome VARCHAR(100),
+    Preco DECIMAL(10,2)
+);
+
+CREATE TABLE Pedidos (
+    PedidoID INT PRIMARY KEY IDENTITY(1,1),
+    ClienteID INT,
+    DataPedido DATE,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
+);
+
+CREATE TABLE ItensPedido (
+    ItemID INT PRIMARY KEY IDENTITY(1,1),
+    PedidoID INT,
+    ProdutoID INT,
+    Quantidade INT,
+    FOREIGN KEY (PedidoID) REFERENCES Pedidos(PedidoID),
+    FOREIGN KEY (ProdutoID) REFERENCES Produtos(ProdutoID)
+);
