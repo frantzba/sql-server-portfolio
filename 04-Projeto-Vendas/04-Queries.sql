21e1af05d8138336237c694e02744ec6e329008c
frantzba <frantzbaguidy7@gmail.com>
2026-03-28 17:41:56 -0300

    Update 04-Queries.sql

--- 04-Projeto-Vendas/04-Queries.sql
+++ 04-Projeto-Vendas/04-Queries.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    c.Nome AS Cliente,
+    p.Nome AS Produto,
+    i.Quantidade,
+    pr.Preco,
+    (i.Quantidade * pr.Preco) AS Total
+FROM ItensPedido i
+JOIN Pedidos p ON i.PedidoID = p.PedidoID
+JOIN Clientes c ON p.ClienteID = c.ClienteID
+JOIN Produtos pr ON i.ProdutoID = pr.ProdutoID;
