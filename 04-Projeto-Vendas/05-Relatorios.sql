721a44f0f18144d1dd66dba054f0ead35afe9541
frantzba <frantzbaguidy7@gmail.com>
2026-03-28 17:45:52 -0300

    Update 05-Relatorios.sql

--- 04-Projeto-Vendas/05-Relatorios.sql
+++ 04-Projeto-Vendas/05-Relatorios.sql
@@ -1,1 +1,9 @@
-
+-- Total por cliente
+SELECT 
+    c.Nome,
+    SUM(i.Quantidade * pr.Preco) AS TotalGasto
+FROM Clientes c
+JOIN Pedidos p ON c.ClienteID = p.ClienteID
+JOIN ItensPedido i ON p.PedidoID = i.PedidoID
+JOIN Produtos pr ON i.ProdutoID = pr.ProdutoID
+GROUP BY c.Nome;
